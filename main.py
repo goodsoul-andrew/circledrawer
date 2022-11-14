9eb952817f3bea44cbf6fdb7e23efaade3c0ee87
goodsoul_andrew <goodsoul.andrew@mail.ru>
2022-11-14 19:23:35 +0500

    UI is imported from CircleDrawer.py and color of circles is random

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from PyQt5.QtCore import Qt, QPoint
 from random import randint
 from PyQt5.QtGui import QPainter, QColor, QPolygon
 from PyQt5.QtWidgets import QWidget, QApplication
+from CircleDrawer import Ui_Form
 
 
-class CircleDrawer(QWidget):
+class CircleDrawer(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
+        self.setWindowTitle('Circles')
         self.figure = 0
         self.coords = [0, 0]
-
-    def initUI(self):
-        self.setGeometry(300, 300, 200, 200)
-        self.setWindowTitle('Circles')
 
     def mousePressEvent(self, event):
         self.coords = [event.x(), event.y()]
@@ -35,10 +33,8 @@ class CircleDrawer(QWidget):
 
     def draw_circle(self, qp):
         x, y = self.coords
-        qp.setPen(QColor(255, 255, 0))
-        qp.setBrush(QColor(255, 255, 0))
-        #qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-        #qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         size = randint(10, 100)
         print(x, y)
         qp.drawEllipse(x, y, size, size)
